3b94ae7e4c836e4b12777b16a305ad7b6a7dc05f
prinzstani <andreas@prinz.no>
2020-02-23 10:46:08 +0100

    auto-generate example buttons

--- Web/sudoku.js
+++ Web/sudoku.js
@@ -1,16 +1,27 @@
 
 const example1 = "6   7 4 1"+" 7   298 "+"     65  "+"73   9  4"+"  261  78"+"    8    "+" 4 96 7 3"+" 8  53 42"+" 5    8  ";
 const example2 = "    3   4"+"16 57 2  "+" 3   1  8"+"  9      "+"78 3 4 95"+"      1  "+"9  6   4 "+"  3 59 27"+"8   1    ";
+const examples = ["6   7 4 1"+" 7   298 "+"     65  "+"73   9  4"+"  261  78"+"    8    "+" 4 96 7 3"+" 8  53 42"+" 5    8  ",
+				  "    3   4"+"16 57 2  "+" 3   1  8"+"  9      "+"78 3 4 95"+"      1  "+"9  6   4 "+"  3 59 27"+"8   1    ",
+				  " 628   3 "+"  5  7  8"+"  4 9   6"+"4    9   "+"  7   5  "+"   2    1"+"8   5 7  "+"5  9  1  "+" 2   465 ",
+				  "4      9 "+"18  5 3  "+"  3   2  "+"   73  1 "+"94   8   "+"7     9  "+" 7   6   "+"  6   7 5"+"8   4    ",
+				  "  9  7 8 "+"83       "+" 1 6    7"+"    48  1"+"  6 3 8  "+"9  15    "+"2    1 6 "+"       54"+" 6 3  7  ",
+				  " 2   1   "+" 5 8   1 "+"   4 23  "+"4    7298"+"5       1"+" 39   4  "+"  3 45 6 "+"         "+"  8 6    ",
+				  "  2 4    "+" 5  7  3 "+"4 3  6  8"+"     95 1"+" 4  8  7 "+"1 67     "+"5  8  2 6"+" 6  3  5 "+"    2 8  "];
+
 setExample1 = function () {
     init(9);
     parseExample(example1, 9);
 }
 
 setExample2 = function () {
-    init(9);
-    parseExample(example2, 9);
+	setExample(2);
 }
 
+setExample = function (idx) {
+    init(9);
+    parseExample(examples[idx], 9);
+}
 
 let sudokuStructure = [];
 
@@ -43,7 +54,19 @@ init = function(size) {
     }
 }
 
-window.addEventListener("load", () => init(9));
+initsudoku = function() {
+    const buttons = document.getElementById("control");
+
+	init(9);
+	for (ex in examples) {
+		const bb = document.createElement("button");
+		bb.innerHTML = "Example " + (parseInt(ex)+1);
+		bb.setAttribute("onClick", "setExample("+ex+")");
+		buttons.appendChild(bb);
+	}
+}
+
+window.addEventListener("load", () => initsudoku());
 
 class Cell {
     constructor(size, htmlElement) {
