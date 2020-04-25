c93d2dd146267251be0b026fd9cbf984f868eb0f
prinzstani <andreas@prinz.no>
2020-04-25 22:26:21 +0200

    cross-check colours if all options are valid

--- Web/sudoku.js
+++ Web/sudoku.js
@@ -470,13 +470,12 @@ class Field {
 	findColours() {
 		// find alternatives for this field
 		if (this.done) return false;
-		console.log("finding colours for " + this.name);
 		const notSolvedCells = this.cells.filter(c => !c.value);
 		let colours = [new Map()];
 		for (const cell of notSolvedCells) {
 			const tempColours = [];
 			for (const colour of colours) {
-				const unusedOptions = cell.options.filter(o => !Array.from(colour.values).includes(o));
+				const unusedOptions = cell.options.filter(o => !Array.from(colour.values()).includes(o));
 				for (const option of unusedOptions) {
 					tempColours.push(new Map(colour).set(cell, option));
 				}
@@ -487,15 +486,13 @@ class Field {
 		this.colours=colours;
 		this.notSolvedCells=notSolvedCells;
 		// checking if we can remove some options
-/*
 		for (let i=0; i<notSolvedCells.length; i++) {
 			let opti=[];
-			for (const colour of colours) opti.push(colour[i]);
+			for (const colour of colours) opti.push(colour.get(notSolvedCells[i]));
 			for (const o of notSolvedCells[i].options) {
 				if (!opti.includes(o)) console.log("### can remove option " + o + " from cell " + notSolvedCells[i].name);
 			}
 		}
-*/
 	}
 
 	alignColours(otherField) {
